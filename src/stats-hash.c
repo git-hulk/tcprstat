928a760bc0cf9f75b264e5b3c5632a7c8aa6a88f
git-hulk <hulk.website@gmail.com>
2015-09-17 17:04:48 +0800

    MOD: modify retrans format

--- src/stats-hash.c
+++ src/stats-hash.c
@@ -23,9 +23,13 @@
 #include <stdint.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 #include <sys/time.h>
 #include <netinet/ip.h>
 #include <netinet/tcp.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
 
 #include "config.h"
 
@@ -39,9 +43,8 @@ struct session {
     struct timeval tv;
     
 	uint32_t id;
-	uint32_t len;
+	uint32_t seq;
     struct session *next;
-	struct tcphdr tcp;
 };
 
 struct hash {
@@ -201,52 +204,81 @@ hash_clean(struct hash *hash, unsigned long min) {
     
 }
 
-static uint32_t EXTRACT_32BITS(const void *p) {
-    return ((uint32_t)ntohl(*(const uint32_t *)(p)));
+static int
+hash_set_session(struct session *sessions, unsigned long sz, 
+		struct session *new)
+{
+	struct session *session;
+    unsigned long port;
+	uint32_t laddr, raddr;
+	uint16_t lport, rport;
+
+	laddr = new->laddr;
+	raddr = new->raddr;
+	lport = new->lport;
+	rport = new->rport;
+    port = hash_fun(laddr, raddr, lport, rport) % sz;
+
+    for (session = sessions + port; session->next; session = session->next) {
+        if (
+            session->next->raddr == raddr &&
+            session->next->laddr == laddr &&
+            session->next->rport == rport &&
+            session->next->lport == lport
+        )
+        {
+            session->next->tv = new->tv;	
+			return 0;
+		}
+	}
+
+    session->next = malloc(sizeof(struct session));
+    if (!session->next)
+        abort();
+    
+    session->next->raddr = raddr;
+    session->next->laddr = laddr;
+    session->next->rport = rport;
+    session->next->lport = lport;
+    session->next->tv = new->tv;
+	session->next->id = new->id;
+	session->next->seq = new->seq;
+
+	return 1;
 }
-void diff_tcp(struct tcphdr *tcp1, struct tcphdr *tcp2) {
-	if(tcp1->seq != tcp2->seq) {
-		printf("diff seq, tcp1: %u, tcp2: %u\n", EXTRACT_32BITS(&tcp1->seq), EXTRACT_32BITS(&tcp2->seq));
-	}
-	if(tcp1->ack_seq != tcp2->ack_seq) {
-		printf("diff seq_ack, tcp1: %u, tcp2: %u\n", EXTRACT_32BITS(&tcp1->ack_seq), EXTRACT_32BITS(&tcp2->ack_seq));
-	}
-	if(tcp1->window != tcp2->window) {
-		printf("diff window\n");
-	}
-	if(tcp1->check != tcp2->check) {
-		printf("diff check\n");
-	}
-	if(tcp1->urg_ptr != tcp2->urg_ptr) {
-		printf("diff urg_ptr\n");
-	}
-	if(tcp1->res1 != tcp2->res1) {
-		printf("diff res1\n");
-	}
-	if(tcp1->res2 != tcp2->res2) {
-		printf("diff res2\n");
-	}
-	if(tcp1->doff!= tcp2->doff) {
-		printf("diff doff\n");
-	}
-	if(tcp1->fin!= tcp2->fin) {
-		printf("diff fin\n");
-	}
-	if(tcp1->syn!= tcp2->syn) {
-		printf("diff syn");
-	}
-	if(tcp1->rst!= tcp2->rst) {
-		printf("diff rst");
-	}
-	if(tcp1->psh != tcp2->psh) {
-		printf("diff psh");
-	}
-	if(tcp1->ack != tcp2->ack) {
-		printf("diff ack");
-	}
-	if(tcp1->urg != tcp2->urg) {
-		printf("diff urg");
-	}
+
+static void
+dump_retrans_session(struct session *session, uint32_t bytes, uint32_t delay)
+{
+	struct timeval tv;
+	time_t nowtime;
+	struct tm *nowtm;
+	char tmbuf[64], buf[64];
+	char laddr[16], raddr[16], *addr;
+	struct in_addr s;
+
+	s.s_addr = session->laddr;
+	addr = inet_ntoa(s);
+    strncpy(laddr, addr, 15);
+    laddr[15] = '\0';
+    
+	s.s_addr = session->raddr;
+    addr = inet_ntoa(s);
+    strncpy(raddr, addr, 15);
+    raddr[15] = '\0';
+
+	gettimeofday(&tv, NULL);
+	nowtime = tv.tv_sec;
+	nowtm = localtime(&nowtime);
+	strftime(tmbuf, sizeof tmbuf, "%Y-%m-%d %H:%M:%S", nowtm);
+	snprintf(buf, sizeof buf, "%s.%06d", tmbuf, (int)tv.tv_usec);
+
+	fprintf(stderr, "[Retrans] [%s] %s:%d <==> %s:%d, after %d ms, length %d bytes.\n", 
+		buf,
+		laddr, session->lport,
+		raddr, session->rport, 
+		delay, bytes
+	);
 }
 
 static int
@@ -272,19 +304,14 @@ hash_set_internal(struct session *sessions, unsigned long sz,
             struct timeval old = session->next->tv;
             session->next->tv = value;
             
-            int diff = (value.tv_sec - old.tv_sec) * 1000 + (value.tv_usec - old.tv_usec) /1000 ;
 			uint32_t id = ip->ip_id;
-			uint32_t len = ntohs(ip->ip_len);	
-			struct tcphdr *old_tcp = &session->next->tcp;
-			if(id != session->next->id && old_tcp->seq == tcp->seq) {
-				// as retrans, tcp header should be the same.
-				if(memcmp(&session->next->tcp, tcp, sizeof(struct tcphdr)) != 0) {
-					diff_tcp(&session->next->tcp, tcp);
-					printf("[ERROR] Tcp header is difference when retrans.\n");
-				}
-            	fprintf(stderr, "[Retrans] %d:%d, after %d ms, length %d bytes.\n", lport, rport, diff, len);
-		 		session->next->id = id;
+			if(id != session->next->id && session->next->seq == tcp->seq) {
+            	int delay;
+				delay = (value.tv_sec - old.tv_sec) * 1000 + (value.tv_usec - old.tv_usec) / 1000;
+				dump_retrans_session(session->next, ntohs(ip->ip_len), delay);
     		}
+
+		 	session->next->id = id;
             return 0;
         }
 	}
@@ -298,8 +325,7 @@ hash_set_internal(struct session *sessions, unsigned long sz,
     session->next->rport = rport;
     session->next->lport = lport;
 	session->next->id = ip->ip_id;
-	session->next->len = ntohs(ip->ip_len);
-	memcpy(&session->next->tcp, tcp, sizeof(struct tcphdr));
+	session->next->seq = tcp->seq;
     
     session->next->tv = value;
     
@@ -311,8 +337,6 @@ hash_set_internal(struct session *sessions, unsigned long sz,
 
 static int
 hash_load_check(struct hash *hash) {
-	hash_newsz(0);
-#if 0
     if ((hash->count * 100) / hash->sz > MAX_LOAD_PERCENT) {
         struct session *new_sessions, *old_sessions;
         unsigned long nsz, i;
@@ -333,11 +357,7 @@ hash_load_check(struct hash *hash) {
             for (session = hash->sessions + i; session->next;
                     session = session->next)
             {
-                
-                hash_set_internal(new_sessions, nsz, session->laddr,
-                        session->raddr, session->lport, session->rport,
-                        session->tv, NULL);
-                        
+                hash_set_session(new_sessions, nsz, session);
             }
             
         }
@@ -351,7 +371,6 @@ hash_load_check(struct hash *hash) {
         return 1;
 
     }
-#endif
     
     return 0;
     
